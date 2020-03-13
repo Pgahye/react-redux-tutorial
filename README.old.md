5e3dd3cc87b0f1a00acb0b3735f8ffe87cd58bc9
ghpark <ghpark@hibrain.net>
2020-03-13 14:02:36 +0900

    Project init, 카운터 컴포넌트 생성

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-redux-tutorial
+리액트 리덕스 튜토리얼 관련 저장소
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counters from './components/Counters';
+
+function App() {
+  return (
+    <div>
+      <Counters number={0} />
+    </div>
+  );
+}
+
+export default App;
