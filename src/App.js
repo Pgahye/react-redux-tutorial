a63af3d8009bee5667d2c81d467259a5e450763b
ghpark <ghpark@hibrain.net>
2020-03-16 15:15:54 +0900

    할일 목록 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Counters from './components/Counters';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div>
       <Counters number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 }
